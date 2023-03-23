01f39764415ff53d45a42a3d3933075e080873ae
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-03-23 16:35:19 +0300

    list view add

--- lib/views/pages/collections.dart
+++ lib/views/pages/collections.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import '../components/texts.dart';
+
+Random random = Random();
+int randomNumber = random.nextInt(100);
 
 class Collections extends StatelessWidget {
   const Collections({
@@ -7,8 +14,42 @@ class Collections extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: titleText(text: 'Your Collention'),
+        backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          collectionTile(),
+          collectionTile(),
+          collectionTile(),
+          collectionTile(),
+          collectionTile(),
+        ],
+      ),
+    );
+  }
+}
+
+class collectionTile extends StatelessWidget {
+  const collectionTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(5.0),
+        child: Image(
+          image: NetworkImage(
+              'https://fastly.picsum.photos/id/1084/200/300.jpg?hmac=JQMQbKvpN6_d6r-fiuOEYe1Dz6f2gfGIkTvsx0nLJUQ'),
+        ),
+      ),
+      title: titleText(size: 20.0, text: "Art title"),
+      subtitle: messageText(text: "Art by Morale taken in 2009 "),
     );
   }
 }
--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -5,6 +5,8 @@ import 'package:ff_project/models/album.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../components/texts.dart';
+
 class Home extends StatelessWidget {
   const Home({
     super.key,
@@ -19,11 +21,47 @@ class Home extends StatelessWidget {
           return ListView.builder(
               itemCount: 5,
               itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Text(data.data![index].author),
-                    Image.network(data.data![index].image),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      titleText(text: data.data![index].author, size: 20.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image(
+                          image: NetworkImage(
+                            data.data![index].image,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            color: primaryColor,
+                            onPressed: () {},
+                            icon: Icon(Icons.favorite),
+                          ),
+                          IconButton(
+                            color: primaryColor,
+                            onPressed: () {},
+                            icon: Icon(Icons.comment),
+                          ),
+                          IconButton(
+                            color: primaryColor,
+                            onPressed: () {},
+                            icon: Icon(Icons.share),
+                          )
+                        ],
+                      ),
+                      const Divider(
+                        endIndent: 10,
+                        indent: 10,
+                        thickness: 1.0,
+                      )
+                    ],
+                  ),
                 );
               });
         } else if (data.hasError) {
